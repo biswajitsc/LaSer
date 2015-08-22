c5751a8a57d588040bea058de8fb717100b36060
Anurag Anand <anuraganandx@gmail.com>
2015-08-22 13:19:17 +0530

    Normalize.sh updated to normalize unicodes

--- Normalization/Normalize.sh
+++ Normalization/Normalize.sh
@@ -1,2 +1,6 @@
 python NumericalNormalization.py
+echo "Normalizing Unicodes. Output will be stored in ../../Data/UnicodeNormalizedMathML"
+rm ../../Data/UnicodeNormalizedMathML
+touch ../../Data/UnicodeNormalizedMathML
+python unicode_normalization.py ../../Data/NumberNormalizedMathML > ../../Data/UnicodeNormalizedMathML
 
