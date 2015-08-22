50fda6ec8f01cbee0a56d7e51d5e521092aebe57
Agnivo <agnivo.saha@gmail.com>
2015-08-22 12:31:22 +0530

    Postings list for unigram, bigram and trigram features created

--- FeatureExtraction/extractFeatures.py
+++ FeatureExtraction/extractFeatures.py
@@ -26,6 +26,7 @@ def main() :
 		for word in words :
 			if (len(word) > 0) :
 				unigrams.add(word)
+	print "Unigrams Extracted"
 	for line in mathML :
 		words = line.split(' ')
 		# print line, len(words)
@@ -35,6 +36,7 @@ def main() :
 				if (len(words[i]) > 0 and len(words[i + 1]) > 0) :
 					bigrams.add((words[i],words[i + 1]))
 				i += 1
+	print "Bigrams Extracted"
 	for line in mathML :
 		words = line.split(' ')
 		if len(words) > 2 :
@@ -44,13 +46,47 @@ def main() :
 				if (len(words[i]) > 0 and len(words[i + 1]) > 0 and len(words[i + 2]) > 0) :	
 					trigrams.add((words[i],words[i + 1],words[i + 2]))
 				i += 1
-
+	print "Trigrams Extracted"
+	unigrams_postinglist = {}
 	for unigram in unigrams :
-		output_file_unigrams.write(unigram + '\n')
+		unigrams_postinglist[unigram] = []
+	i = 0
+	for line in mathML :
+		i += 1
+		for unigram in unigrams :
+			if unigram in line :
+				unigrams_postinglist[unigram].append(i)
+	print "Unigram Features Postings List created"
+	bigrams_postinglist = {}
 	for bigram in bigrams :
-		output_file_bigrams.write(str(bigram) + '\n')
+		bigrams_postinglist[bigram] = []
+	i = 0
+	for line in mathML :
+		i += 1
+		if (i % 100 == 0) :
+			print str(i) + "th xml checked for bigrams"
+		for bigram in bigrams :
+			if (str(bigram[0]) + ' ' + str(bigram[1])) in line :
+				bigrams_postinglist[bigram].append(i)
+	print "Bigram Features Postings List created"
+	trigrams_postinglist = {}
 	for trigram in trigrams :
-		output_file_trigrams.write(str(trigram) + '\n')
+		trigrams_postinglist[trigram] = []
+	i = 0
+	for line in mathML :
+		i += 1
+		if (i % 100 == 0) :
+			print str(i) + "th xml checked for trigrams"
+		for trigram in trigrams :
+			if (str(trigram[0]) + ' ' + str(trigram[1]) + ' ' + str(trigram[2])) in line :
+				trigrams_postinglist[trigram].append(i)
+	print "Trigram Features Postings List created"	
+	for unigram in unigrams :
+		output_file_unigrams.write("{" + str(unigram) + " : " + str(unigrams_postinglist[unigram]) + "}" + '\n')
+	for bigram in bigrams :
+		output_file_bigrams.write("{" + str(bigram) + " : " + str(bigrams_postinglist[bigram]) + "}" + '\n')
+	for trigram in trigrams :
+		output_file_trigrams.write("{" + str(trigram) + " : " + str(trigrams_postinglist[trigram]) + "}" + '\n')
 
 if __name__ == "__main__" :
 	main()
