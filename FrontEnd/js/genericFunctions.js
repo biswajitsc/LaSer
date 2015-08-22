b858fca05cba6b6b5fdc00a17d2b713c407536bc
prithm <pritspido@gmail.com>
2015-08-22 15:52:16 +0530

    updated Frontend

---
+++ FrontEnd/js/genericFunctions.js
@@ -0,0 +1,20 @@
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+function httpGet(theUrl,mathSnippet)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl+mathSnippet , false ); // false for synchronous request
+    xmlHttp.send();
+    return xmlHttp.responseText;
+}
