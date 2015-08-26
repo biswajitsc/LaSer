3856f7e194efde32a3bea7aff585a0b5e6e5cfd9
Anurag Anand <anuraganandx@gmail.com>
2015-08-26 20:42:52 +0530

    Operator Grouping added

--- Normalization/Normalize.sh
+++ Normalization/Normalize.sh
@@ -1,6 +1,8 @@
 python NumericalNormalization.py
+echo "Operator Grouping. Output will be stored in ../../Data/OperatorGroupedMathML"
+rm ../../Data/OperatorGroupedMathML
+python operator_grouping.py ../../Data/NumberNormalizedMathML > ../../Data/OperatorGroupedMathML
 echo "Normalizing Unicodes. Output will be stored in ../../Data/UnicodeNormalizedMathML"
 rm ../../Data/UnicodeNormalizedMathML
-touch ../../Data/UnicodeNormalizedMathML
-python unicode_normalization.py ../../Data/NumberNormalizedMathML > ../../Data/UnicodeNormalizedMathML
+python unicode_normalization.py ../../Data/OperatorGroupedMathML > ../../Data/UnicodeNormalizedMathML
 
