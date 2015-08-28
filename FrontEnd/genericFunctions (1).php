e61f7bfb2a0d382f75b71890e50aa50503bc10cc
Agnivo <agnivo.saha@gmail.com>
2015-08-29 01:07:55 +0530

    updated

---
+++ FrontEnd/genericFunctions (1).php
@@ -0,0 +1,39 @@
+<?php
+
+function httpGet($theUrl,$mathSnippet)
+{
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $theUrl.'/'.$mathSnippet);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    $result2 = curl_exec($curl);
+    $result3 = json_decode($result2,true);
+
+    return $result3;
+    
+ //    foreach ($result3 as $key => $value) {
+ //        echo $value['original_eqn'];
+ //    }
+
+
+ //    //echo $result3["1"]["score"];
+ //    $result = '[';
+ //    for($i = 0; $i < 4; $i ++) {
+ //    	$result .= '{';
+ //    	$result .= '"docID":"docID-'.$i.'",';
+ //    	$result .= '"docLink":"docLink-'.$i.'",';
+ //    	$result .= '"equation":"';
+ //    	$result .= '<math xmlns:m=\"http://www.w3.org/1998/Math/MathML\" display=\"block\">   <mrow>     <mi mathvariant=\"normal\">Λ</mi>     <mo>=</mo>     <mrow>       <mfenced open=\"(\" close=\")\">         <mrow>           <mi>T</mi>           <mo>⁢</mo>           <mi>T</mi>         </mrow>       </mfenced>       
+ //    	<mo>-</mo>       <mrow>         <mfrac>           <mn>3</mn>           <mn>10</mn>         </mfrac>         <mo>⁢</mo>         <mrow>           
+ // 	<msup>             <mo>∂</mo>             <mn>2</mn>           </msup>           <mo>⁡</mo>           <mi>T</mi>         </mrow>       </mrr
+	// ow>     </mrow>   </mrow> </math>';
+	// 	$result .= '"}';
+
+ //    	if($i < 3) $result .= ',';
+ //    }
+ //    $result .= ']';
+ //    $resultJson = json_decode($result, true);
+ //    return $resultJson;  
+}
+
+?>
--- FrontEnd/index (1).php
+++ FrontEnd/index (1).php
@@ -1,10 +1,10 @@
 <?php
     include_once 'genericFunctions.php';
 ?>
- 
+
 <!DOCTYPE html>
 <meta charset="utf-8">
- 
+
 <head>
   <title>LaSer</title>
   <link rel="stylesheet" href="css/bootstrap.min.css">
@@ -12,73 +12,73 @@
   <link rel="stylesheet" href="css/index.css">
   <script src="js/jquery-1.11.0.min.js"></script>
 </head>
- 
+
 <nav id="navBar" class="navbar navbar-static" role="navigation">
-        <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                  <span class="sr-only">Toggle navigation</span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.php">LaSer</a>
-        </div>
- 
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                  <li class= "active" ><a href="index.php">Home</a></li>
-                  <li class= "" ><a href="about.php">About</a></li>
-                </ul>
-        </div>
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		  <span class="sr-only">Toggle navigation</span>
+		  <span class="icon-bar"></span>
+		  <span class="icon-bar"></span>
+		  <span class="icon-bar"></span>
+		</button>
+		<a class="navbar-brand" href="index.php">LaSer</a>
+	</div>
+
+	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		<ul class="nav navbar-nav">
+		  <li class= "active" ><a href="index.php">Home</a></li>
+		  <li class= "" ><a href="about.php">About</a></li>
+		</ul>
+	</div>
 </nav>
- 
+
 <body>
- 
-        <div class="container">
-               
-                <div class="row">
-                <div class="col-sm-6 col-sm-offset-3">
-                    <div id="imaginary_container">
-                        <div class="input-group stylish-input-group">
-                        <form id="mathSnippetForm" action="index.php">
-                            <input type="text" name = 'mathSnippet' class="form-control" placeholder="Search" >
-                        </form>    
-                        <span class="input-group-addon">
-                                <button id="submitButton" type="submit">
-                                <span class="glyphicon glyphicon-search"></span>
-                                </button>  
-                        </span>
-                        </div>
-                    </div>
-                </div>
-                </div>
- 
-                <div class="ranked-results">
- 
+
+	<div class="container">
+		
+		<div class="row">
+	        <div class="col-sm-6 col-sm-offset-3">
+	            <div id="imaginary_container"> 
+	                <div class="input-group stylish-input-group">
+	                <form id="mathSnippetForm" action="index.php">
+	                    <input type="text" name = 'mathSnippet' class="form-control" placeholder="Search" >
+	                </form>    
+	                <span class="input-group-addon">
+	                   	<button id="submitButton" type="submit">
+	                       	<span class="glyphicon glyphicon-search"></span>
+	                   	</button>  
+	                </span>
+	                </div>
+	            </div>
+	        </div>
+		</div>
+
+		<div class="ranked-results">
+
             <?php
                 $url = "localhost:8080"; //Put the url here
                 $mathSnippet = $_GET['mathSnippet'];
                 $mathSnippet = urlencode($mathSnippet);
                 $mathSnippet = str_replace('+', '%20', $mathSnippet);
                 $resultJson = httpGet($url,$mathSnippet);
- 
+
                 foreach ($resultJson as $key => $value) {
                     // echo $value['original_eqn'];
                     echo '<div class="hr-line-dashed"></div>';
                     echo '<div class="search-result">';
                     //echo $value['docID'];
                     echo '<center><a href='.'http://arxiv.org/abs/hep-th/'.$value['original_doc_id'].' target="_blank">'.$value['original_doc_id'].'</a></center>';
-                    echo '<center><div>'.$value['original_eqn'].'</div></center>';
-                    echo '<center><div>'.$value['score'].'</div></center>';
+                    echo $value['original_eqn'];
+                    echo $value['score'];
                     echo '</div>';
                 }
- 
+
                 echo '<div class="hr-line-dashed"></div>';
             ?>
-                       
-                </div>
- 
-                <div id="paginationDiv" class="text-center">
+			
+		</div>
+
+		<div id="paginationDiv" class="text-center">
             <div class="btn-group">
                 <button class="btn btn-white">First</button>
                 <button class="btn btn-white" type="button"><i class="glyphicon glyphicon-backward"></i></button>
@@ -95,17 +95,17 @@
                 <button class="btn btn-white">Last</button>
             </div>
         </div>
- 
+
     </div>
- 
+
     <script type="text/javascript">
-   
+    
     $('#submitButton').click(function() {
     $( "#mathSnippetForm" ).submit();
     });
- 
+
     </script>
- 
- 
-   
+
+
+    
 </body>
