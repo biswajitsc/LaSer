d51d3c890131a00eb832b2bfdcbefd49e2effb53
prithm <pritspido@gmail.com>
2015-10-06 16:44:37 +0530

    Updated code for structural norm

--- Normalization/genTreeStructure.py
+++ Normalization/genTreeStructure.py
@@ -1,5 +1,5 @@
 import xml.etree.ElementTree as ET
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 import sys
 
 
@@ -19,14 +19,13 @@ def reduceExpression(terminalXml):
 			tempString += ' * '
 		else:
 			tempString += ' ' + terminalXmlText + ' '
-		tempString = '</'+terminalXml.tag+'>'
+		tempString += '</'+terminalXml.tag+'>'
 		variations.append(tempString)
 
 	if terminalXml.tag == 'mi' or terminalXml.tag == 'mn':
-		variations.append('<'+'mi'+'> '+'Variable'+' </'+'mi'+'>')
+		variations.append('<'+'mi'+'> '+'Expression'+' </'+'mi'+'>')
 		variations.append('<'+terminalXml.tag+'> '+terminalXmlText+' </'+terminalXml.tag+'>')
 
-	variations.append(' </Expression> ')
 
 	return variations
 
@@ -50,35 +49,51 @@ def genTreeStructureUtil(rawXml):
 	for i in xrange(0,len(variations)):
 		variations[i] = '<' + rawXml.tag + '> ' + variations[i] + ' </' + rawXml.tag + '>'
 
-	variations.append(' </Expression> ')
+	variations.append('<mi> Expression </mi>')
 
 	return variations
 		
-def genTreeStructure(mathmlXml,mathmlMeta):
-	meta = open(mathmlMeta, 'r').readlines()
-	structureMathML = open('../../Data/StructureMathML.xml', 'w')
-	structureMathMLMeta = open('../../Data/StructureMathMLMeta.xml', 'w')
-	
-	mathXmlFile = open(mathmlXml, 'r')
+def genTreeStructure(mathmlXml):
+	# meta = open(mathmlMeta, 'r').readlines()
+	# structureMathML = open('../../Data/StructureMathML.xml', 'w')
+	# structureMathMLMeta = open('../../Data/StructureMathMLMeta.xml', 'w')
+	# mathXmlFile = open(mathmlXml, 'r')
 	cnt = -1
-	for rawEquation in mathXmlFile:
+	for rawEquation in mathmlXml:
 		cnt += 1
 		print rawEquation.replace('m:','')
 		variations = genTreeStructureUtil(ET.fromstring(rawEquation.replace('m:','')))
+		variations = variations[:-1]
 		for variation in variations:
-			structureMathML.write(str(cnt+1) + ' ' + variation.encode('utf-8') + '\n')
-			structureMathMLMeta.write(meta[cnt].strip('\n')+'\n')
-
+			# structureMathML.write(str(cnt+1) + ' ' + variation.encode('utf-8') + '\n')
+			# structureMathMLMeta.write(meta[cnt].strip('\n')+'\n')
+			print variation
+			# print 
 		if cnt >= 0:
 			break
 	
-	structureMathML.close()
-	structureMathMLMeta.close()
-	mathXmlFile.close()
+	# structureMathML.close()
+	# structureMathMLMeta.close()
+	# mathXmlFile.close()
 
 def main():
-	genTreeStructure('../../Data/MathML.xml','../../Data/MathMLMeta.xml')
-	
+	# genTreeStructure('../../Data/MathML.xml','../../Data/MathMLMeta.xml')
+	genTreeStructure([\
+	"<math>\
+		<msup>\
+			<mi>e</mi>\
+			<mrow>\
+				<mo>{</mo>\
+				<mrow>\
+					<mi>x</mi>\
+					<mo>+</mo>\
+					<mi>y</mi>\
+				</mrow>\
+			<mo>}</mo>\
+			</mrow>\
+		</msup>\
+	</math>"\
+	])
 
 if __name__ == '__main__':
 	main()
