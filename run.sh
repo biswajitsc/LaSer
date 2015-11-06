6bfec700b3c970611cabb24e194688ef900bf37d
Anurag Anand <anuraganandx@gmail.com>
2015-11-06 20:08:53 +0530

    run.sh added

---
+++ run.sh
@@ -0,0 +1,9 @@
+head -$1 ../Data/MathMLMeta.xml > ../Data/smallMathMLMeta.xml
+head -$1 ../Data/MathML.xml > ../Data/smallMathML.xml
+cd ExtractData
+python SimplifyEquations.py ../../Data/smallMathML.xml ../../Data/smallMathMLMeta.xml
+cd ../Normalization
+python Normalization.py
+cd ../FeatureExtraction
+python extractExpressionFeatures.py ../../Data/NormalizedMathML.xml
+python extractContextFeatures.py ../../Data/equation_context.txt
