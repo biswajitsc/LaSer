ca2b07e2da7e7f59319ba69ae0d8ea634f652532
prithm <pritspido@gmail.com>
2015-11-14 15:10:40 +0530

    Updated Context Extraction

--- ExtractData/ExtractContextEqn.py
+++ ExtractData/ExtractContextEqn.py
@@ -7,7 +7,7 @@ import nltk
 from nltk.corpus import stopwords
 stop = stopwords.words('english')
 
-windowSize = 20
+windowSize = 10
 	
 def filterText(inp):
 	inp = re.sub('\W+',' ',inp)
@@ -28,57 +28,75 @@ def parse(data,eqn):
 	#print data
 	data = data.replace(eqn,' SETSUNA_DAISUKI ')
 	eqn = 'SETSUNA_DAISUKI'
+	# ind = data.find(eqn)
 	tokens = data.split()
 	res = {}
 	ids = []
 
+	print 'here parse'
+
 	for i in xrange(0, len(tokens)):
 		if tokens[i] == eqn:
 			ids.append(i)
-
-	for i in xrange(0, len(tokens)):
-		if i in ids:
+	print len(ids)
+	for i in ids:
+		# if i%1000 == 0:
+		# 	print i,len(tokens)
+		# while ptr < len(ids) and i < ids[ptr]:
+		# 	ptr += 1
+		# if ptr == len(ids):
+		# 	break
+		if True:
+			if i % 1000 == 0:
+				print i
 			before = ""
 			for j in xrange(1, windowSize):
 				if i - j < 0 or tokens[i - j].startswith('\\end'):
 					break
-				before = tokens[i - j] + ' ' + before
-			before = before[:-1]
+				if len(before) > 0:
+					before += ' '
+				before += tokens[i - j]
 			after = ""
 			for j in xrange(1, windowSize):
 				if i + j >= len(tokens) or tokens[i + j].startswith('\\begin'):
 					break
 				after += tokens[i + j] + ' '
 			after = after[:-1]
-
+			before = before[::-1]
 			if eqn in res.keys():	
 				res[eqn] = res[eqn] + ' ' + before + ' ' + after
 			else:
 				res[eqn] = before + ' ' + after
+
+	# print 'return res'
+
 	return res
 
 def main():
-	meta = open('../../Data/Meta').read().decode('cp1252', errors='ignore').split('\n')
-	equations = open('../../Data/Formulae').read().decode('cp1252', errors='ignore').split('\n')
+	meta = open('../../Data/MathMLMeta.xml').read().decode('cp1252', errors='ignore').split('\n')
+	equations = open('../../Data/MathML.xml').read().decode('cp1252', errors='ignore').split('\n')
 
 	skipped = 0
 	cnt = 0
 	out = codecs.open('../../Data/Context/eqncontext.txt', 'w', 'cp1252')
 
 	for i in xrange(0, len(meta)):
+		print cnt
 		doc = meta[i]
 		eqn = equations[i]
-		cnt += 1
+		# cnt += 1
 		if cnt % 100 == 0:
 			print "Done ", cnt, "Skipped ", skipped
 		try:
+			# print '1'
 			data = open('../../Dataset/{0}/{1}'.format(doc.split(' ')[0],doc.split(' ')[1]), 'r').read().decode('cp1252', errors='ignore')
 			data = data.replace('\n','  ')
-
+			# print '2'
 			res = parse(data,eqn)
 			if 'SETSUNA_DAISUKI' not in res.keys():
 				skipped += 1
 				continue
+			print '3'
 			out.write(str(i) + ' ' + filterText(res['SETSUNA_DAISUKI']) + '\n')
 			out.flush()
 						
